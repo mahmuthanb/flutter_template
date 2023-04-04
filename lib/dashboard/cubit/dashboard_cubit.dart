64d62716feb5812ed1048beb6b63f5f88cbd69d8
Mahmuthan <mahmuthanb@gmail.com>
2023-04-04 13:31:30 +0300

    dashboard cubit and state

--- lib/dashboard/cubit/dashboard_cubit.dart
+++ lib/dashboard/cubit/dashboard_cubit.dart
@@ -2,9 +2,9 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 part 'dashboard_state.dart';
 
-class DashboardCubit extends Cubit<int> {
-  DashboardCubit() : super(0);
+class DashboardCubit extends Cubit<DashboardState> {
+  DashboardCubit() : super(DashboardInitial());
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
+  void increment() => emit(state);
+  void decrement() => emit(state);
 }
