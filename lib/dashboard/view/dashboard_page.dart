01c90bea5e8b7212cf02a79ce03f99329f1979ea
Mahmuthan <mahmuthanb@gmail.com>
2023-04-07 16:16:01 +0300

    dashboard ui

--- lib/dashboard/view/dashboard_page.dart
+++ lib/dashboard/view/dashboard_page.dart
@@ -43,20 +43,13 @@ class DashboardView extends StatelessWidget {
             loading: LoadingPlaceholder.new,
           ),
           floatingActionButton: state.maybeWhen(
-            initial: () => Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  onPressed: () => context.read<DashboardCubit>().increment(),
-                  child: const Icon(Icons.add),
-                ),
-                const SizedBox(height: 8),
-                FloatingActionButton(
-                  onPressed: () => context.read<DashboardCubit>().decrement(),
-                  child: const Icon(Icons.remove),
-                ),
-              ],
+            initial: () => FloatingActionButton(
+              onPressed: () => context.read<DashboardCubit>().increment(),
+              child: const Icon(Icons.abc),
+            ),
+            loading: () => FloatingActionButton(
+              onPressed: () => context.read<DashboardCubit>().decrement(),
+              child: const Icon(Icons.live_help),
             ),
             orElse: () {
               return null;
@@ -75,6 +68,6 @@ class CounterText extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final count = context.select((DashboardCubit cubit) => cubit.state);
-    return Text('$count', style: theme.textTheme.displayLarge);
+    return Text('$count', style: theme.textTheme.displaySmall);
   }
 }
