54abe09740fef8957b6346c78cffef0d6cbeb494
Mahmuthan <mahmuthanb@gmail.com>
2023-04-17 16:40:33 +0300

    settings page ui updates

--- lib/features/settings/view/settings_page.dart
+++ lib/features/settings/view/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_template/core/res/colors.dart';
 import 'package:flutter_template/core/res/dimens.dart';
 import 'package:flutter_template/l10n/l10n.dart';
 
@@ -17,14 +18,14 @@ class SettingsPage extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            color: Colors.blueGrey,
+            color: AppColors.grey,
             height: size.height * .1,
             child: Row(
               children: [
                 Expanded(
                   child: Container(
                     padding: const EdgeInsets.all(AppDimens.s),
-                    color: Colors.lime,
+                    // color: Colors.lime,
                     child: const CircleAvatar(
                       backgroundImage: CachedNetworkImageProvider(
                         'https://picsum.photos/200',
@@ -36,8 +37,22 @@ class SettingsPage extends StatelessWidget {
                 Expanded(
                   flex: 4,
                   child: Container(
-                    color: Colors.red,
-                    child: const Text('data'),
+                    // color: Colors.red,
+                    child: RichText(
+                      text: const TextSpan(
+                        text: 'Welcome, \n',
+                        // style: DefaultTextStyle.of(context).style,
+                        children: <TextSpan>[
+                          TextSpan(
+                            text: 'Mahmuthan',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          TextSpan(
+                            text: '\nYou can set up your preferences here.',
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 )
               ],
